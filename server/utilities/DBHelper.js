df6919241152e0f5f75e3d936bbfad440db763a6
Zachary Donato <zdonato@stevens.edu>
2017-09-27 15:59:08 -0400

    Adds user creation and retrieval methods.

--- server/utilities/DBHelper.js
+++ server/utilities/DBHelper.js
@@ -14,7 +14,11 @@ let mysql = require('mysql'),
 
 const SALT_ROUNDS = 10;
 const CONNECTION_LIMIT = 100;
+const ERRORS = require('./errors');
 
+/**
+ * DBHelper class.
+ */
 class DBHelper {
 
     /**
@@ -25,4 +29,119 @@ class DBHelper {
         this._pool = mysql.createPool(config);
     }
 
+    /**
+     * Method to add a new user to the database.
+     * @param fname {String} - The first name of the user
+     * @param lname {String} - The last name of the user
+     * @param email {String} - The user's email, also functions as their username
+     * @param password {String} - The user's password
+     * @param dob {String} - A string of the user's date of a birth
+     * 
+     * @return Returns a promise that resolves with the created user's id or error.
+     */
+    createUser(fname, lname, email, password, dob) {
+        return new Promise( (resolve, reject) => {
+            // Check for undefined required vars.
+            if (_.isUndefined(email) || _.isUndefined(password)) {
+                logError('DBHelper.createUser', 'Email and password must be defined', undefined);
+                reject(ERRORS.UNDEFINED_VAL_USER);
+                return;
+            }
+
+            ts(`[DBHelper.createUser] Adding user ${email}`);
+            this._pool.getConnection( (err, connection) => {
+                if (err) {
+                    logError('DBHelper.createUser', 'Error getting connection from pool', err);
+                    reject(ERRORS.DB_CONN_ERR);
+                    return;
+                }
+
+                // First check if the user with this email already exists.
+                this.getUserByEmail(email)
+                    .then( (result) => {
+                        // User was found, reject the attempt to create a new one.
+                        logError('DBHelper.createUser this.getUserByEmail', `User already exists with email ${email}`);
+                        reject(ERRORS.USER_TAKEN);
+                        return;
+                    })
+                    .catch( (err) => {
+                        // No user exists, create the new account.
+                        let sql = 'INSERT INTO `Accounts` (FirstName`, `LastName`, `email`, `password`, dob`) VALUES (?, ?, ?, ?, ?)';
+
+                        bcrypt.hash(password, SALT_ROUNDS, (err, hash) => {
+                            if (err) {
+                                logError('DBHelper.createUser bcrypt.hash', 'Error generating hash', err);
+                                reject(ERRORS.CREATE_USER_ERR);
+                                return;
+                            }
+
+                            connection.query(sql, [fname, lname, email, hash, dob], (err, result) => {
+                                // Release connection.
+                                connection.release();
+
+                                if (err) {
+                                    logError('DBHelper.createUser connection.query', 'Error adding user to database', err);
+                                    reject(ERRORS.CREATE_USER_ERR);
+                                    return;
+                                }
+
+                                // User created successfully. Return the user object.
+                                this.getUserByEmail(email)
+                                    .then( (result) => { resolve(result); })
+                                    .catch( (err) => { reject(err); });
+                            });
+                        });
+                    });
+            });
+        })
+    }
+
+    /**
+     * Method to retrieve a user's information by email.
+     * @param email {String} - The user's email
+     * 
+     * @return Returns a promise that resolves with the a user object
+     */
+    getUserByEmail(email) {
+        return new Promise( (resolve, reject) => {
+            const sql = 'SELECT UserId, email, FirstName, LastName, dob FROM Accounts WHERE email = ?';
+            ts(`[DBHelper.getUserByEmail] getting user ${email}`);
+            
+            this._pool.getConnection( (err, connection) => {
+                if (err) {
+                    logError('DBHelper.getUserByEmail this._pool.getConnection', 'Error getting connection from pool', err);
+                    reject(ERRORS.DB_CONN_ERR);
+                    return;
+                }
+
+                // Query for the user.
+                connection.query(sql, email, (err, result) => {
+                    // Release connection.
+                    connection.release();
+
+                    if (err) {
+                        logError('DBHelper.getUserByEmail connection.query', 'Error retrieving user information', err);
+                        reject(ERRORS.USER_INFO_ERR);
+                        return;
+                    }
+
+                    if (_.isEmpty(result)) {
+                        logError('DBHelper.getUserByEmail', 'No user exists with this email');
+                        reject(ERRORS.NO_USER_FOUND);
+                        return;
+                    }
+
+                    resolve(result);
+                });
+            });
+        });
+    }
 }
+
+function logError(fnName, msg, error) {
+    ts(`ERROR: [${fnName}] ${msg}`);
+
+    if (error) console.log(error);
+}
+
+export DBHelper;
---
+++ server/utilities/errors.js
@@ -0,0 +1,30 @@
+/**
+ * An object containing common error messages.
+ */
+
+module.exports = {
+    DB_CONN_ERR: {
+        error: 'Error connecting to the database',
+        code: 500
+    },
+    USER_INFO_ERR: {
+        error: 'Error retrieving user information',
+        code: 500
+    },
+    NO_USER_FOUND: {
+        error: 'No user was found with this email',
+        code: 404
+    },
+    USER_TAKEN: {
+        error: 'A user already exists with this email',
+        code: 403
+    },
+    CREATE_USER_ERR: {
+        error: 'Error creating user',
+        code: 500
+    },
+    UNDEFINED_VAL_USER: {
+        error: 'Email and password must be defined',
+        code: 400
+    }
+}
