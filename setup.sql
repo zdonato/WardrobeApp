60e617fdcd62ebc957a49668e7cd20ae3ba5101e
Zachary Donato <zdonato@stevens.edu>
2017-11-22 18:25:08 -0500

    Adds a basic sql setup script to create the Accounts database and a user to access it.

---
+++ setup.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE WardrobeApp;
+USE WardrobeApp;
+
+CREATE TABLE Accounts (
+    userId int NOT NULL AUTO_INCREMENT,
+    firstName varchar(32),
+    lastName varchar(32),
+    email varchar(64),
+    password varchar(32),
+    dob varchar(32),
+    PRIMARY KEY (userId)
+);
+
+CREATE USER 'wardrobe_app_main'@'localhost' IDENTIFIED BY 'seniord';
+GRANT ALL PRIVILEGES ON * . * TO 'wardrobe_app_main'@'localhost';
+FLUSH PRIVILEGES;
+
